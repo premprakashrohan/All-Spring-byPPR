59e08fe35425a83f3e450b2ece7997792dc4f2d0
premprakashrohan <premprakashrohan@gmail.com>
2021-06-12 20:12:52 +0530

    Command runner

--- src/main/java/com/ps/all/AllSpringApplication.java
+++ src/main/java/com/ps/all/AllSpringApplication.java
@@ -1,13 +1,22 @@
 package com.ps.all;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AllSpringApplication {
+public class AllSpringApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AllSpringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello from spring boot command line runner!!!!");
+		if(args.length>0)
+		for(int i =0; i<=args.length;i++)System.out.println(args[i]);
+		
 	}
 
 }
